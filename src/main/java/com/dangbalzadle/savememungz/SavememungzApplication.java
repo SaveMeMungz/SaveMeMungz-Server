736df32d38a662eb8dc8e7540d6fc5ffc3c9e285
SHIMHUN <shimhun88@naver.com>
2024-09-26 14:13:53 +0900

    초기 세팅

---
+++ src/main/java/com/dangbalzadle/savememungz/SavememungzApplication.java
@@ -0,0 +1,13 @@
+package com.dangbalzadle.savememungz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavememungzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavememungzApplication.class, args);
+	}
+
+}
