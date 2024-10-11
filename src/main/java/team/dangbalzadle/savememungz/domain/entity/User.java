d64948613cff912e37caefa89f9217720ba716ac
SHIMHUN <shimhun88@naver.com>
2024-10-11 17:24:27 +0900

    :sparkles: User 엔티티 추가

---
+++ src/main/java/team/dangbalzadle/savememungz/domain/entity/User.java
@@ -0,0 +1,21 @@
+package team.dangbalzadle.savememungz.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import team.dangbalzadle.savememungz.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
